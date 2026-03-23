11ddb15453531fb8a5056f3805218b0e24aaa9e4
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2026-03-23 19:04:24 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='app'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/auth' element={<Auth />} />
